9453ede7b4e89be8ea5f0ea36135c68adec7f888
kalninandre <andrekalnin@gmail.com>
2022-05-23 16:42:17 -0300

    Main Header Title Change

--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -4,7 +4,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
     return (
         <header className={classes.header}>
-            <h1>ReduxCart</h1>
+            <h1>Redux Cart Application</h1>
             <nav>
                 <ul>
                     <li>
